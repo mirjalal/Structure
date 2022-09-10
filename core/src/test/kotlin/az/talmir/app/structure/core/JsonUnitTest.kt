e2a6a3a081aeb44922ae0b40203ee0a2681d4927
mirjalal <mirjalal.talishinski@gmail.com>
2022-09-10 23:19:23 +0400

    update: add tests json & koin dependencies together

--- core/src/test/kotlin/az/talmir/app/structure/core/JsonUnitTest.kt
+++ core/src/test/kotlin/az/talmir/app/structure/core/JsonUnitTest.kt
@@ -1,6 +1,6 @@
 package az.talmir.app.structure.core
 
-import az.talmir.app.structure.core.features.auth.AuthResponse
+import az.talmir.app.structure.core.features.auth.sign_in_models.SignInResponse
 import az.talmir.app.structure.core.koin.commonModule
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
@@ -24,27 +24,27 @@ class JsonUnitTest : KoinTest {
     @Test
     fun `test string parsing to AuthRemoteResponse without refresh_token returns true`() {
         val strJson = """{"access_token":"blahblah"}"""
-        val obj = json.decodeFromString<AuthResponse>(strJson)
-        assertEquals(AuthResponse(access_token = "blahblah"), obj)
+        val obj = json.decodeFromString<SignInResponse>(strJson)
+        assertEquals(SignInResponse(access_token = "blahblah"), obj)
     }
 
     @Test
     fun `test string parsing to AuthRemoteResponse with refresh_token returns true`() {
         val strJson = """{"access_token":"blahblah", "refresh_token":"qweqwe"}"""
-        val obj = json.decodeFromString<AuthResponse>(strJson)
-        assertEquals(AuthResponse(access_token = "blahblah", refresh_token = "qweqwe"), obj)
+        val obj = json.decodeFromString<SignInResponse>(strJson)
+        assertEquals(SignInResponse(access_token = "blahblah", refresh_token = "qweqwe"), obj)
     }
 
     @Test
     fun `test AuthRemoteResponse parsing with nullable prop value returns true`() {
-        val obj = AuthResponse(access_token = "blah_blah", refresh_token = null)
+        val obj = SignInResponse(access_token = "blah_blah", refresh_token = null)
         val strJson = json.encodeToString(obj)
         assertEquals("""{"access_token":"blah_blah"}""", strJson)
     }
 
     @Test
     fun `test AuthRemoteResponse parsing without nullable prop value returns true`() {
-        val obj = AuthResponse(access_token = "blah_blah", refresh_token = "qwe_qwe")
+        val obj = SignInResponse(access_token = "blah_blah", refresh_token = "qwe_qwe")
         val strJson = json.encodeToString(obj)
         assertEquals("""{"access_token":"blah_blah","refresh_token":"qwe_qwe"}""", strJson)
     }
@@ -52,7 +52,7 @@ class JsonUnitTest : KoinTest {
     @Test
     fun `test parsing unknown keys returns true`() {
         val strJson = """{"access_token":"blahblah", "some_new_key":123}"""
-        val obj = json.decodeFromString<AuthResponse>(strJson)
-        assertEquals(AuthResponse(access_token = "blahblah", refresh_token = null), obj)
+        val obj = json.decodeFromString<SignInResponse>(strJson)
+        assertEquals(SignInResponse(access_token = "blahblah", refresh_token = null), obj)
     }
 }
