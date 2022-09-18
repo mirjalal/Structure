461207647402ad74fd6ab89a9a88673e29426ce5
mirjalal <mirjalal.talishinski@gmail.com>
2022-09-18 15:25:40 +0400

    update: add token custom expire time millis

--- core/src/main/kotlin/az/talmir/app/structure/core/storage/prefs/token/TokenInfo.kt
+++ core/src/main/kotlin/az/talmir/app/structure/core/storage/prefs/token/TokenInfo.kt
@@ -1,7 +1,16 @@
 package az.talmir.app.structure.core.storage.prefs.token
 
 import az.talmir.app.structure.core.helpers.Local
+import kotlinx.datetime.Clock.System
 import kotlinx.serialization.Serializable
 
+private const val JWT_EXPIRE_MILLIS = 15 * 60 * 1_000L
+private const val REFRESH_TOKEN_EXPIRE_MILLIS = 86_400L * 1_000L
+
 @Serializable
-data class TokenInfo(val jwt: String, val refreshToken: String? = null) : Local()
+data class TokenInfo(
+    val jwt: String,
+    val jwtExpireAt: Long = System.now().toEpochMilliseconds() + JWT_EXPIRE_MILLIS,
+    val refreshToken: String? = null,
+    val refreshTokenExpireAt: Long = jwtExpireAt - JWT_EXPIRE_MILLIS + REFRESH_TOKEN_EXPIRE_MILLIS
+) : Local()
