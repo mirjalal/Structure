6100327975f8ae674da35ac1e84860b675590d31
mirjalal <mirjalal.talishinski@gmail.com>
2022-09-18 15:30:27 +0400

    update: replace `TokenInfoReaderService` w/ `TokenInfoRepository`

--- core/src/main/kotlin/az/talmir/app/structure/core/bridges/TokenizedApiBridge.kt
+++ core/src/main/kotlin/az/talmir/app/structure/core/bridges/TokenizedApiBridge.kt
@@ -1,15 +1,15 @@
 package az.talmir.app.structure.core.bridges
 
+import az.talmir.app.structure.core.features.token.TokenInfoRepository
 import az.talmir.app.structure.core.helpers.ApiCallParams
 import az.talmir.app.structure.core.helpers.doApiCall
 import az.talmir.app.structure.core.storage.prefs.language.LangReaderService
-import az.talmir.app.structure.core.storage.prefs.token.TokenInfoReaderService
 import az.talmir.app.structure.shared.models.Result
 import io.ktor.client.statement.HttpResponse
 import kotlinx.coroutines.Dispatchers
 
 class TokenizedApiBridge(
-    val tokenReaderService: TokenInfoReaderService,
+    val tokenInfoRepository: TokenInfoRepository,
     val langReaderService: LangReaderService
 ) {
     suspend inline fun <reified T> buildApiCall(
@@ -18,7 +18,7 @@ class TokenizedApiBridge(
         doApiCall(Dispatchers.IO) {
             request(
                 ApiCallParams(
-                    token = tokenReaderService.getTokenInfo()?.jwt,
+                    token = tokenInfoRepository.getJwt()?.jwt,
                     language = langReaderService.getLangCode()
                 )
             )
