4b750afa09719b35213f477508808e7b474964d6
mirjalal <mirjalal.talishinski@gmail.com>
2022-09-28 16:25:32 +0400

    fix: replace dummy api link

--- core/src/test/kotlin/az/talmir/app/structure/core/KoinUnitTest.kt
+++ core/src/test/kotlin/az/talmir/app/structure/core/KoinUnitTest.kt
@@ -5,27 +5,35 @@ import az.talmir.app.structure.core.koin.KoinCoreModule
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
 import kotlin.test.assertNotNull
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.junit.Assert.assertEquals
-import org.junit.Rule
 import org.junit.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 import org.koin.ksp.generated.module
 import org.koin.test.KoinTest
-import org.koin.test.KoinTestRule
 import org.koin.test.inject
 
 class KoinUnitTest : KoinTest {
     private val json by inject<Json>()
     private val httpClient by inject<HttpClient>()
 
-    @get:Rule
-    val koinTestRule = KoinTestRule.create {
-        printLogger()
-        modules(KoinCoreModule().module)
+    @BeforeTest
+    fun setup() {
+        startKoin {
+            modules(KoinCoreModule().module)
+        }
+    }
+
+    @AfterTest
+    fun tearDown() {
+        stopKoin()
     }
 
     @Test
@@ -39,7 +47,7 @@ class KoinUnitTest : KoinTest {
     @Test
     fun `test HttpClient is OK`() {
         runBlocking {
-            assertNotNull(httpClient.get("https://api.ipify.org/?format=json").body())
+            assertNotNull(httpClient.get("https://jsonplaceholder.typicode.com/todos/1").body())
         }
     }
 }
