43c27aceaaf93063cc8e68276618baf356ea5a5a
mirjalal <mirjalal.talishinski@gmail.com>
2022-09-28 11:46:59 +0400

    update: add basic root & auth activities

---
+++ app/src/main/kotlin/az/talmir/app/structure/screens/auth/AuthActivity.kt
@@ -0,0 +1,15 @@
+package az.talmir.app.structure.screens.auth
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+        }
+    }
+}
