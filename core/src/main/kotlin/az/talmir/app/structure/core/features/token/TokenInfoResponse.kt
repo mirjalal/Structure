189f2199902b9889b8504fd626de07a8d28dbf10
mirjalal <mirjalal.talishinski@gmail.com>
2022-11-01 10:35:49 +0400

    update: add expire time info

--- core/src/main/kotlin/az/talmir/app/structure/core/features/token/TokenInfoResponse.kt
+++ core/src/main/kotlin/az/talmir/app/structure/core/features/token/TokenInfoResponse.kt
@@ -1,6 +1,20 @@
 package az.talmir.app.structure.core.features.token
 
+import kotlinx.datetime.Clock
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class TokenInfoResponse(val accessToken: String, val refreshToken: String?)
+data class TokenInfoResponse(
+    val accessToken: String,
+    val jwtExpireAt: Long,
+    val refreshToken: String?,
+    val refreshTokenExpireAt: Long,
+) {
+    fun isValid(): Boolean {
+        val now = Clock.System.now().toEpochMilliseconds()
+        return if (refreshToken == null)
+            jwtExpireAt >= now
+        else
+            now in (jwtExpireAt + 1) until refreshTokenExpireAt
+    }
+}
